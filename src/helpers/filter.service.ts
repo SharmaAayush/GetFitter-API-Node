80fca225e9d2d918b68ed62c013cfd6365bba741
Ayush Sharma <shandilya.aayush@gmail.com>
2026-06-30 23:29:13 +0530

    Feat: memoize filter responses

--- src/helpers/filter.service.ts
+++ src/helpers/filter.service.ts
@@ -2,7 +2,7 @@ import { transformModelArr } from "@/services/util";
 import { errAsync, okAsync } from "neverthrow";
 import logger from "@/services/logger";
 import { Model, ModelStatic } from "sequelize";
-import { IModelWithTransformation } from "@/types/base.models";
+import { BaseModelResponse, IModelWithTransformation } from "@/types/base.models";
 import { FilterModelResponse } from "@/types/filter.model";
 import { ERROR_REASONS } from "@/consts/error-reasons";
 
@@ -10,7 +10,12 @@ export abstract class FilterService {
   abstract className: string;
   abstract model: ModelStatic<Model & IModelWithTransformation<FilterModelResponse>>;
 
+  private cachedResponse: BaseModelResponse[] = [];
+
   async getAll() {
+    if (this.cachedResponse.length > 0) {
+      return okAsync(this.cachedResponse);
+    }
     try {
       const records = await this.model.findAll({
         order: [['name', 'ASC']], // Order by name in ascending order
@@ -18,6 +23,7 @@ export abstract class FilterService {
 
       const data = await transformModelArr(records);
 
+      this.cachedResponse = data;
       return okAsync(data);
     } catch (error) {
       logger.error(`${this.className}.getAll: Error fetching records`);
